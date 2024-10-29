52ed38e92ec5595f1f589e60a88f95f9d9544238
designdoughdev <dev@designdough.co.uk>
2024-10-29 16:18:27 +0000

    add menu btn

--- header.php
+++ header.php
@@ -19,20 +19,25 @@
 
     <div class="main-header container">
 
-        <div class="header-logo-container">
+        <div class="main-header-left">
             <img class="header-logo" src="<?php bloginfo('template_url'); ?>/assets/images/svg/logo.svg" alt="">
 
         </div>
 
-        <div class="menu-btn-container">
-            <button>One</button>
-            <button>Two</button>
-            <button>Three</button>
+        <div class="main-header-centre">
+            <button class="btn-menu">Discover</button>
+            <button class="btn-menu">Explore</button>
+            <button class="btn-menu">Do</button>
         </div>
-        <div class="menu-btn-container">
-            <button class="md:hidden ms-auto" aria-label="Menu" @click="isOpen = !isOpen">
-                Menu
-            </button>
+        <div class="main-header-right">
+
+            <div class="hamburger-icon-container">
+                <button class="md:hidden ms-auto hamburger-btn" aria-label="Menu" @click="isOpen = !isOpen">
+                    <img class="hamburger-icon" src="<?php bloginfo('template_url'); ?>/assets/images/svg/hamburger.svg"
+                        alt="menu">
+                </button>
+            </div>
+
 
             <?php get_template_part('components/includes/mobile-nav'); ?>
         </div>
