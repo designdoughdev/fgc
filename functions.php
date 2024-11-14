1fe1e273ec7803d1be56bf01d47eb9f6f52190b0
designdoughdev <dev@designdough.co.uk>
2024-11-14 10:45:52 +0000

    remove plugins

--- functions.php
+++ functions.php
@@ -146,7 +146,6 @@ function add_file_types_to_uploads($file_types)
 }
 add_filter('upload_mimes', 'add_file_types_to_uploads');
 
-
 //------------------------ Plugin Management -------------------------------//
 
 add_action('tgmpa_register', 'dd_theme_register_required_plugins');
@@ -174,45 +173,21 @@ function dd_theme_register_required_plugins()
             'slug'      => 'contact-form-7',
             'required'  => false,  // Optional plugin
         ),
-        // AltText.ai - Recommended plugin from the WordPress repository
-        array(
-            'name'      => 'AltText.ai',
-            'slug'      => 'alttext-ai',
-            'required'  => false,  // Optional plugin
-        ),
-        // WP Mail SMTP - Recommended plugin from the WordPress repository
-        array(
-            'name'      => 'WP Mail SMTP',
-            'slug'      => 'wp-mail-smtp',
-            'required'  => false,  // Optional plugin
-        ),
-        // CookieYes | GDPR Cookie Consent - Recommended plugin from the WordPress repository
-        array(
-            'name'      => 'CookieYes | GDPR Cookie Consent',
-            'slug'      => 'cookie-law-info',
-            'required'  => false,  // Optional plugin
-        ),
-        // Flamingo - Recommended plugin from the WordPress repository
-        array(
-            'name'      => 'Flamingo',
-            'slug'      => 'flamingo',
-            'required'  => false,  // Optional plugin
-        ),
+
     );
 
     $config = array(
-        'id'           => 'dd_theme',                 // Unique ID for hashing notices for multiple instances of TGMPA
-        'default_path' => '',                         // Default absolute path to bundled plugins
-        'menu'         => 'tgmpa-install-plugins',    // Menu slug
-        'parent_slug'  => 'themes.php',               // Parent menu slug
-        'capability'   => 'edit_theme_options',       // Capability needed to view plugin install page
-        'has_notices'  => true,                       // Show admin notices or not
-        'dismissable'  => true,                       // Dismissable by user
+        'id'           => 'dd_theme',
+        'default_path' => '',
+        'menu'         => 'tgmpa-install-plugins',
+        'parent_slug'  => 'themes.php',
+        'capability'   => 'edit_theme_options',
+        'has_notices'  => true,
+        'dismissable'  => true,
     );
 
     tgmpa($plugins, $config);
 }
-
 
 // Automatically activate required plugins after theme switch
 function dd_theme_activate_required_plugins()
