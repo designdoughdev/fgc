568d773fc0bbd3392ec8648e6f48dc85c56aebfb
designdoughdev <dev@designdough.co.uk>
2024-11-14 11:57:26 +0000

    remove file

--- components/flex/content-text_and_image.php
+++ components/flex/content-text_and_image.php
@@ -31,29 +31,183 @@ $layout = get_query_var('layout'); ?>
         <?php if ($layout == 'full-colour'): ?>
 
 
-            <!-------------------------- Layout Full Colour --------------------------------->
-            <div class="section-content full-colour-layout relative blue-style">
+        <!-------------------------- Layout Full Colour --------------------------------->
+        <div class="section-content full-colour-layout relative blue-style">
 
 
 
-                <div class="col">
-                    <p class="title-tag">international</p>
-                    <h3 class="heading h2 text-white">Empowering Change Worldwide</h3>
-                    <p class="body text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor
-                        incididunt ut labore et dolore magna aliqua ut enim ad.</p>
-                    <a href="/" class="link btn text-white sky-blue">Visit Page</a>
-                </div>
+            <div class="col">
+                <p class="title-tag">international</p>
+                <h3 class="heading h2 text-white">Empowering Change Worldwide</h3>
+                <p class="body text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor
+                    incididunt ut labore et dolore magna aliqua ut enim ad.</p>
+                <a href="/" class="link btn text-white sky-blue">Visit Page</a>
+            </div>
 
-                <?php
+            <?php
                 echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
                 ?>
 
-            </div>
+        </div>
 
-            <!---------------------------------------------------------------------->
+        <!---------------------------------------------------------------------->
 
         <?php elseif ($layout == 'big-img-no-bg'): ?>
+
+        <!-------------------------- Layout Big Image No BG --------------------------------->
+        <div class="section-content big-img-no-bg-layout relative mint-style" data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom">
+
+
+
+
+            <p class="title-tag">Cymru Can5</p>
+            <div class="text-content">
+                <h3 class="heading h2">The innovative goals paving the way for a stronger, greener Wales</h3>
+                <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut
+                    labore et
+                    dolore magna aliqua ut enim ad.</p>
+                <a href="/" class="link btn text-white">Cymru Can</a>
+
+            </div>
+
+
+            <div class="img-wrap">
+                <img src=<?php echo get_template_directory_uri() . "/assets/images/jpg/tree.jpg" ?> alt="">
+            </div>
+
+            <div class="vertical-bars-container">
+
+                <?php
+                    // small version
+                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars-small.svg');
+                    // large version
+                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
+                    ?>
+
+            </div>
+
+
+        </div>
+
+        <!---------------------------------------------------------------------->
+
+
+
+
+        <!-------------------------- Layout Rows --------------------------------->
+
+        <?php elseif ($layout == 'rows'): ?>
+        <div class="section-content rows-layout relative blue-style" data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom">
+
+
+
+
+            <p class="title-tag">Well-being of Future
+                Generations (Wales) Act 2015</p>
+            <h3 class="heading h2">Shaping a brighter tomorrow: Discover how the Well-being of Future Generations Act is
+                transforming Wales</h3>
+            <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et
+                dolore magna aliqua ut enim ad.</p>
+            <a href="/" class="link btn text-white">About the Act</a>
+
+            <div class="img-wrap">
+                <img src=<?php echo get_template_directory_uri() . "/assets/images/jpg/talk.jpg" ?> alt="">
+            </div>
+
+            <div class="vertical-bars-container">
+
+                <?php
+                    // small version
+                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
+                    // large version
+                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
+                    ?>
+
+            </div>
+
+
+        </div>
+
+        <?php elseif ($layout == 'half-image'): ?>
+
+        <!-------------------------- Layout Half image --------------------------------->
+        <div class="section-content half-image-layout relative mint-style" data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom">
+
+
+            <div class="text-content">
+                <p class="title-tag">Meet the commissioner</p>
+                <h3 class="heading h2">Creating a legacy for Future Generations</h3>
+                <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua ut enim ad.</p>
+                <a href="/" class="link btn">About the Act</a>
+
+            </div>
+
+
+
+            <div class="img-wrap">
+                <img class="landscape-img"
+                    src=<?php echo get_template_directory_uri() . "/assets/images/jpg/derek-crop.jpg" ?> alt="">
+                <img class="portrait-img"
+                    src=<?php echo get_template_directory_uri() . "/assets/images/jpg/derek-crop.jpg" ?> alt="">
+            </div>
+
+            <div class="bars-container">
+
+                <?php
+                    // horizontal bars
+                    echo file_get_contents(get_template_directory() . '/assets/images/svg/horizontal-bars-small.svg');
+                    // vertical bars
+                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
+                    ?>
+
+
+
+                <div class="caption light-navy">
+                    <p class="bold">Derek Walker</p>
+                    <p>Future Generations Commissioner for Wales</p>
+                </div>
+
+            </div>
+
+            <?php elseif ($layout == 'full-image'): ?>
+
+            <!-------------------------- Layout Full Image --------------------------------->
+            <div class="section-content full-image-layout relative yellow-style" data-aos="fade-up"
+                data-aos-anchor-placement="top-bottom"
+                style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/jpg/viaduct.jpg' ?>') ;">
+
+
+                <div class="text-content">
+                    <p class="title-tag">Meet the commissioner</p>
+                    <h3 class="heading h2">Creating a legacy for Future Generations</h3>
+                    <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua ut enim ad.</p>
+                    <a href="/" class="link btn text-white">About the Act</a>
+
+                </div>
+
+
+
+                <div class="bars-container">
+
+                    <?php
+                        // horizontal bars
+                        echo file_get_contents(get_template_directory() . '/assets/images/svg/horizontal-bars.svg');
+                        ?>
+
+                </div>
+
+
+            </div>
+
+            <?php elseif ($layout == 'big-img-no-bg'): ?>
 
             <!-------------------------- Layout Big Image No BG --------------------------------->
             <div class="section-content big-img-no-bg-layout relative mint-style" data-aos="fade-up"
@@ -62,7 +216,7 @@ $layout = get_query_var('layout'); ?>
 
 
 
-                <p class="title-tag">Cymru Can5</p>
+                <p class="title-tag">Cymru Can</p>
                 <div class="text-content">
                     <h3 class="heading h2">The innovative goals paving the way for a stronger, greener Wales</h3>
                     <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
@@ -75,183 +229,29 @@ $layout = get_query_var('layout'); ?>
 
 
                 <div class="img-wrap">
-                    <img src=<?php echo get_template_directory_uri() . "/assets/images/jpg/tree.jpg" ?> alt="">
+                    <img src=<?php echo get_template_directory_uri() . "https://picsum.photos/2000/1333" ?> alt="">
                 </div>
 
                 <div class="vertical-bars-container">
 
                     <?php
-                    // small version
-                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars-small.svg');
-                    // large version
-                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
-                    ?>
-
-                </div>
-
-
-            </div>
-
-            <!---------------------------------------------------------------------->
-
-
-
-
-            <!-------------------------- Layout Rows --------------------------------->
-
-        <?php elseif ($layout == 'rows'): ?>
-            <div class="section-content rows-layout relative blue-style" data-aos="fade-up"
-                data-aos-anchor-placement="top-bottom">
-
-
-
-
-                <p class="title-tag">Well-being of Future
-                    Generations (Wales) Act 2015</p>
-                <h3 class="heading h2">Shaping a brighter tomorrow: Discover how the Well-being of Future Generations Act is
-                    transforming Wales</h3>
-                <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et
-                    dolore magna aliqua ut enim ad.</p>
-                <a href="/" class="link btn text-white">About the Act</a>
-
-                <div class="img-wrap">
-                    <img src=<?php echo get_template_directory_uri() . "/assets/images/jpg/talk.jpg" ?> alt="">
-                </div>
-
-                <div class="vertical-bars-container">
-
-                    <?php
-                    // small version
-                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars-block.svg');
-                    // large version
-                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
-                    ?>
-
-                </div>
-
-
-            </div>
-
-        <?php elseif ($layout == 'half-image'): ?>
-
-            <!-------------------------- Layout Half image --------------------------------->
-            <div class="section-content half-image-layout relative mint-style" data-aos="fade-up"
-                data-aos-anchor-placement="top-bottom">
-
-
-                <div class="text-content">
-                    <p class="title-tag">Meet the commissioner</p>
-                    <h3 class="heading h2">Creating a legacy for Future Generations</h3>
-                    <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua ut enim ad.</p>
-                    <a href="/" class="link btn">About the Act</a>
-
-                </div>
-
-
-
-                <div class="img-wrap">
-                    <img class="landscape-img"
-                        src=<?php echo get_template_directory_uri() . "/assets/images/jpg/derek-crop.jpg" ?> alt="">
-                    <img class="portrait-img"
-                        src=<?php echo get_template_directory_uri() . "/assets/images/jpg/derek-crop.jpg" ?> alt="">
-                </div>
-
-                <div class="bars-container">
-
-                    <?php
-                    // horizontal bars
-                    echo file_get_contents(get_template_directory() . '/assets/images/svg/horizontal-bars-small.svg');
-                    // vertical bars
-                    echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
-                    ?>
-
-
-
-                    <div class="caption light-navy">
-                        <p class="bold">Derek Walker</p>
-                        <p>Future Generations Commissioner for Wales</p>
-                    </div>
-
-                </div>
-
-            <?php elseif ($layout == 'full-image'): ?>
-
-                <!-------------------------- Layout Full Image --------------------------------->
-                <div class="section-content full-image-layout relative yellow-style" data-aos="fade-up"
-                    data-aos-anchor-placement="top-bottom"
-                    style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/jpg/viaduct.jpg' ?>') ;">
-
-
-                    <div class="text-content">
-                        <p class="title-tag">Meet the commissioner</p>
-                        <h3 class="heading h2">Creating a legacy for Future Generations</h3>
-                        <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua ut enim ad.</p>
-                        <a href="/" class="link btn text-white">About the Act</a>
-
-                    </div>
-
-
-
-                    <div class="bars-container">
-
-                        <?php
-                        // horizontal bars
-                        echo file_get_contents(get_template_directory() . '/assets/images/svg/horizontal-bars.svg');
-                        ?>
-
-                    </div>
-
-
-                </div>
-
-            <?php elseif ($layout == 'big-img-no-bg'): ?>
-
-                <!-------------------------- Layout Big Image No BG --------------------------------->
-                <div class="section-content big-img-no-bg-layout relative mint-style" data-aos="fade-up"
-                    data-aos-anchor-placement="top-bottom">
-
-
-
-
-                    <p class="title-tag">Cymru Can</p>
-                    <div class="text-content">
-                        <h3 class="heading h2">The innovative goals paving the way for a stronger, greener Wales</h3>
-                        <p class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                            incididunt ut
-                            labore et
-                            dolore magna aliqua ut enim ad.</p>
-                        <a href="/" class="link btn text-white">Cymru Can</a>
-
-                    </div>
-
-
-                    <div class="img-wrap">
-                        <img src=<?php echo get_template_directory_uri() . "https://picsum.photos/2000/1333" ?> alt="">
-                    </div>
-
-                    <div class="vertical-bars-container">
-
-                        <?php
                         // small version
                         echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars-small.svg');
                         // large version
                         echo file_get_contents(get_template_directory() . '/assets/images/svg/vertical-bars.svg');
                         ?>
 
-                    </div>
-
-
                 </div>
 
 
             </div>
 
-            <!---------------------------------------------------------------------->
 
-            <!---------------------------------------------------------------------->
+        </div>
+
+        <!---------------------------------------------------------------------->
+
+        <!---------------------------------------------------------------------->
         <?php endif; ?>
 
 
