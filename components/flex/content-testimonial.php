97b60a25a858e52d1eade70c1cc3ddb6e4d69344
dd-adam <adam@designdough.co.uk>
2024-11-16 14:38:10 +0000

    finish testimonials flex

---
+++ components/flex/content-testimonial.php
@@ -0,0 +1,30 @@
+<?php $colourScheme = get_sub_field('colour_scheme'); ?>
+<?php $small_title = get_sub_field('small_title'); ?>
+<?php $quote = get_sub_field('quote'); ?>
+<?php $quote_info = get_sub_field('quote_info'); ?>
+
+<?php $row = get_row_index() - 0; ?>
+
+<section class="section_testimonial  row-<?php echo $row; ?> relative">
+    <div class="container">
+
+        <div class="section-content <?php echo $colourScheme ?>-style testimonial">
+
+            <div class="text-content">
+                <?php if ($small_title): ?>
+                <p class="title-tag"><?php echo $small_title; ?></p>
+                <?php endif; ?>
+                <?php if ($quote): ?>
+                <h4 class="quote h2"><?php echo $quote; ?></h4>
+                <?php endif; ?>
+                <?php if ($quote_info): ?>
+                <p class="quote-info"><?php echo $quote_info; ?></p>
+                <?php endif; ?>
+
+            </div>
+
+
+        </div>
+
+    </div>
+</section>
