cbb03bdf58eac5e1989787b824d92490a1caf506
dd-adam <adam@designdough.co.uk>
2024-11-21 12:29:25 +0000

    add icon version

--- components/flex/content-info_cards.php
+++ components/flex/content-info_cards.php
@@ -4,6 +4,7 @@ $add_background = get_sub_field('add_background');
 $smallTitle = get_sub_field('small_title');
 $bigTitle = get_sub_field('big_title');
 $body = get_sub_field('body');
+$style = get_sub_field('style');
 $infoCards = get_sub_field('selected_posts');
 ?>
 
@@ -24,31 +25,35 @@ $infoCards = get_sub_field('selected_posts');
             <h2 class="title-tag"><?php echo $smallTitle; ?></h2>
             <?php endif; ?>
 
+            <?php if ($bigTitle || $body): ?>
+
             <div class="top-section">
                 <div class="title-wrap">
 
                     <?php if ($bigTitle): ?>
                     <h3 class="heading"><?php echo $bigTitle; ?></h3>
                     <?php endif; // big title 
-                    ?>
+                        ?>
                 </div>
                 <?php if ($body): ?>
                 <p class="body"><?php echo $body; ?></p>
                 <?php endif; // body 
-                ?>
+                    ?>
             </div>
+            <?php endif; ?>
 
             <!-------------------------- Info cards/tiles --------------------------------->
 
 
             <div class="cards-section">
 
-                <?php $colourSchemes = ['mint', 'blue', 'yellow']; ?>
+                <?php $colourSchemes = ['blue', 'yellow', 'mint']; ?>
 
                 <?php while (have_rows('cards')) : the_row(); ?>
 
                 <?php $colourScheme = $colourSchemes[(get_row_index() - 1) % 3]; ?>
 
+                <?php $icon = get_sub_field('icon'); ?>
                 <?php $cardTitle = get_sub_field('card_title'); ?>
                 <?php $cardBody = get_sub_field('card_body'); ?>
 
@@ -57,10 +62,26 @@ $infoCards = get_sub_field('selected_posts');
                 <div class="info-card <?php echo $colourScheme; ?>-scheme">
 
                     <div class="text-content">
+                        <?php if ($style == 'icons'): ?>
+
+
+                        <div class="icon-container">
+
+                            <?php if ($icon): ?>
+
+                            <?php
+                                        echo file_get_contents(get_template_directory() . '/assets/images/svg/' . $icon . '-icon.svg');
+                                        ?>
+                            <?php endif; ?>
+
+                        </div>
+                        <?php else:  // numbered style 
+                            ?>
                         <p class="tag card-index"><?php if (get_row_index() < 10) {
-                                                            echo 0;
-                                                        }
-                                                        echo get_row_index(); ?></p>
+                                                                echo 0;
+                                                            }
+                                                            echo get_row_index(); ?></p>
+                        <?php endif; ?>
                         <h4 class="card-title"><?php echo $cardTitle; ?></h4>
                         <p class="card-body"><?php echo $cardBody; ?></p>
 
