e8de691a011a758729ac4bb8b15574e29cb6b964
dd-adam <adam@designdough.co.uk>
2024-12-03 13:58:57 +0000

    add full width mobile version to testimonial and text dynamic

--- components/flex/content-text_dynamic.php
+++ components/flex/content-text_dynamic.php
@@ -1,5 +1,5 @@
 <section class="section_text_dynamic">
-    <div class="container">
+    <div class="container-wrapper">
         <div class="section-wrapper">
             <div class="section-content">
 
@@ -8,16 +8,16 @@
                 <div class="thumbnail_nav">
                     <!-- repeater -->
                     <?php if (have_rows('content_rows')) : ?>
-                    <?php $index = 0; ?>
-                    <?php while (have_rows('content_rows')) : the_row(); ?>
+                        <?php $index = 0; ?>
+                        <?php while (have_rows('content_rows')) : the_row(); ?>
 
-                    <?php $slide_title = get_sub_field('slide_title'); ?>
+                            <?php $slide_title = get_sub_field('slide_title'); ?>
 
-                    <button class="thumbnail <?php if ($index == 0) { ?>active<?php } ?>"
-                        data-index="<?php echo $index; ?>"><?php echo $slide_title; ?></button>
-                    <?php $index++; ?>
+                            <button class="thumbnail <?php if ($index == 0) { ?>active<?php } ?>"
+                                data-index="<?php echo $index; ?>"><?php echo $slide_title; ?></button>
+                            <?php $index++; ?>
 
-                    <?php endwhile; ?>
+                        <?php endwhile; ?>
                     <?php endif; ?>
                 </div>
 
@@ -27,19 +27,19 @@
                     <div class="splide__track">
                         <div class="splide__list">
                             <?php if (have_rows('content_rows')) : ?>
-                            <?php while (have_rows('content_rows')) : the_row(); ?>
-                            <?php
+                                <?php while (have_rows('content_rows')) : the_row(); ?>
+                                    <?php
                                     // get slide text body
                                     $text_body = get_sub_field('text_body');
                                     ?>
-                            <div class="splide__slide">
+                                    <div class="splide__slide">
 
-                                <div class="text_body_wrap">
-                                    <p class="slider-text-body"><?php echo $text_body; ?></p>
-                                </div>
-                            </div>
+                                        <div class="text_body_wrap">
+                                            <p class="slider-text-body"><?php echo $text_body; ?></p>
+                                        </div>
+                                    </div>
 
-                            <?php endwhile; ?>
+                                <?php endwhile; ?>
                             <?php endif; ?>
                         </div>
                     </div>
